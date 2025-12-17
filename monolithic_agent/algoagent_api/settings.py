72eefcbc7adacf68b920dbe3e445f617d08e92fb
Chiqo-ke <nyagamacharia3@gmail.com>
2025-12-18 02:18:44 +0300

    fix: Correct ALLOWED_HOSTS entry for development environment

--- monolithic_agent/algoagent_api/settings.py
+++ monolithic_agent/algoagent_api/settings.py
@@ -45,7 +45,7 @@ SECRET_KEY = 'django-insecure-ic)b377zj54kad&)o__t(#)8l&i-4n664+nw3j8_u&-q00ux12
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'testserver', 'ps283t0p-8080.uks1.devtunnels.ms']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'testserver', 'ps283t0p-8000.uks1.devtunnels.ms']
 
 
 # Application definition
